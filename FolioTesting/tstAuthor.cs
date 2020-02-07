95e118cc18e35fe077fccfb64c168ba52abd9852
hughalunjones <hughalunjones12@gmail.com>
2020-02-07 13:54:47 +0000

    Added testing and a class for Author

---
+++ FolioTesting/tstAuthor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FolioClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FolioTesting
+{
+    [TestClass]
+    public class tstAuthor
+    {
+        [TestMethod]
+        public void InstanceOf()
+        {
+            clsAuthor AnAuthor = new clsAuthor();
+            Assert.IsNotNull(AnAuthor);
+        }
+    }
+}
