b7820e7a671161e6e22ef7ab4b9fd88a2624c596
Emmanuel Adedeji <P2400857@my365.dmu.ac.uk>
2020-02-14 14:11:49 +0000

    Added testing and class for Order Records

---
+++ FolioTesting/tstOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FolioClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FolioTesting
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
